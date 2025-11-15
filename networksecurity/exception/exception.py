c71e593cd010359d16925bd0d034155874aa9441
anurag <onuverma04@gmail.com>
2025-11-16 00:28:33 +0530

    Data Ingestion component completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,27 +1,22 @@
 import sys
 from networksecurity.logging import logger
 
-def NetworkSecurityException(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info() ## this variable will have the error details like in which file the error has occured ,error line number
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message='Error occurred in the python script name [{0}] line number [{1}] error message [{2}]'.format(file_name,exc_tb.tb_lineno,str(error))
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
     
-    return error_message
-    
-
-
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=NetworkSecurityException(error_message,error_detail=error_detail)
-
     def __str__(self):
-        return self.error_message
-    
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
+        
 if __name__=='__main__':
     try:
-        logger.logging.info('enter the try block')
+        logger.logging.info("Enter the try block")
         a=1/0
-        print('this will not be printed',a)
+        print("This will not be printed",a)
     except Exception as e:
-        raise CustomException(e,sys)
+           raise NetworkSecurityException(e,sys)
